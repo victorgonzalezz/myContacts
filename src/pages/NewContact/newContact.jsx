dcb5a187aecd55f455af17b3659a8e49db9fe391
Victor Gonzalez <vitorpcg@gmail.com>
2023-10-02 17:10:58 +0100

    input and select
    input and select done(static)

--- src/pages/NewContact/newContact.jsx
+++ src/pages/NewContact/newContact.jsx
@@ -1,9 +1,18 @@
 import PageHeader from '../../components/PageHeader/pageHeader';
+import Input from '../../components/input';
+import Select from '../../components/select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
