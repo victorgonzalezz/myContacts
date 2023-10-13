ac1d47a8c9902d6177cf7f213238a49bf5554b57
Victor Gonzalez <vitorpcg@gmail.com>
2023-10-13 18:05:15 +0100

    validation and custom Hooks
    validation, errors, and custom Hooks

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,4 +1,9 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+
+import useErrors from '../../hooks/useErrors';
 
 import { Form, ButtonContainer } from './styles';
 
@@ -8,23 +13,83 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+
+  function handleNomeChange(event) {
+    setNome(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'nome', message: 'Nome é obrigatório.' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'Tá errado o email seu tanso!' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      nome, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    // eslint-disable-next-line react/jsx-no-bind
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('nome')}>
+        <Input
+          error={getErrorMessageByFieldName('nome')}
+          nome="nome"
+          placeholder="Nome"
+          value={nome}
+          // eslint-disable-next-line react/jsx-no-bind
+          onChange={handleNomeChange}
+        />
       </FormGroup>
       <FormGroup
-        error="Campo e-email formato inválido"
+        error={getErrorMessageByFieldName('email')}
       >
-        <Input placeholder="E-mail" error />
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          placeholder="E-mail"
+          nome="email"
+          value={email}
+          // eslint-disable-next-line react/jsx-no-bind
+          onChange={handleEmailChange}
+        />
       </FormGroup>
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          nome="phone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Lista</option>
           <option value="instagran">Instagran</option>
+          <option value="category">Category</option>
         </Select>
       </FormGroup>
 
